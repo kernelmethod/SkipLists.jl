0e0a340f2ca35e7ec866a50a5c3ea22ebcb2100f
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-06-06 02:34:13 -0600

    Add some tests for making multithreaded deletes from a Skiplist.

--- test/test_list_concurrency.jl
+++ test/test_list_concurrency.jl
@@ -4,10 +4,13 @@ Concurrency tests for the Skiplist type
 
 =======================================================#
 
-using Skiplists, Test
+using Random, Skiplists, Test
+using Base.Iterators: partition
 using Base.Threads: @spawn
 
 @testset "SkiplistNode concurrency tests" begin
+    Random.seed!(0)
+
     @testset "Multithreaded insert into Skiplist" begin
         list = Skiplist{Int64}()
         tasks = []
@@ -26,4 +29,35 @@ using Base.Threads: @spawn
         @test length(list) == 30
         @test vec(list) == collect(1:30)
     end
+
+    @testset "Multithreaded delete from Skiplist" begin
+        list = Skiplist{Int64}()
+
+        # Insert 1, 2, ..., 40
+        for ii = 1:40
+            insert!(list, ii)
+        end
+
+        @test vec(list) == collect(1:40)
+
+        # Delete all of the even numbers in separate threads
+        to_delete = filter(iseven, shuffle(1:40))
+        to_delete = partition(to_delete, 10)
+        tasks = []
+        for (ii, to_delete_partition) in enumerate(to_delete)
+            rng = MersenneTwister(ii)
+            task = @spawn begin
+                for val in to_delete_partition
+                    delete!(list, val)
+                    sleep(0.1 * rand(rng))
+                end
+            end
+            push!(tasks, task)
+        end
+
+        wait.(tasks)
+        @test vec(list) == collect(1:2:40)
+        @test length(list) == collect(1:2:40) |> length
+    end
 end
+
