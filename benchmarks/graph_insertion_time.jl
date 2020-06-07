8244d2c8dde348a77709094ad4748ea2a7b6e57b
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-06-07 12:52:28 -0600

    Add a script to plot the time required to insert a new element into the Skiplist.

---
+++ benchmarks/graph_insertion_time.jl
@@ -0,0 +1,56 @@
+#============================================
+
+Graph the amount of time required to insert a new element into
+a Skiplist.
+
+============================================#
+
+pushfirst!(LOAD_PATH, joinpath(@__DIR__, ".."))
+
+using BenchmarkTools
+using Logging
+using PyPlot
+using Skiplists
+
+pygui(true)
+
+list_sizes = (100_000, 120_000, 140_000, 160_000)
+
+# Helper functions
+
+function generate_list(N)
+    list = Skiplist{Int64}()
+    for ii = 1:N
+        insert!(list, ii)
+    end
+    list
+end
+
+randint(N) = abs(mod(rand(Int64), N))
+
+# Plotting script
+
+results = []
+
+for N in list_sizes
+    push!(results, @benchmark(
+        insert!(list, ii),
+        setup=(list = generate_list($N); ii = randint($N);),
+        seconds=10,
+       ))
+    @info "Obtained benchmarks for N = $N"
+end
+
+@info "Benchmarks completed"
+
+rcParams = PyPlot.PyDict(PyPlot.matplotlib."rcParams")
+rcParams["font.size"] = 15
+
+times = [median(r.times) for r in results]
+plot(list_sizes, times)
+xlabel("List size")
+ylabel("Insertion time (μs)")
+show()
+
+
+
